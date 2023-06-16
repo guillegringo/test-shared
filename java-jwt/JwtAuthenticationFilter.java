83352d9e00c81cc987b63e101f91a87cb9bc470d
Guillermo Wortmann <guillermo.wortmann@gmail.com>
2023-06-16 15:11:50 -0300

    opa

--- java-jwt/JwtAuthenticationFilter.java
+++ java-jwt/JwtAuthenticationFilter.java
@@ -1,43 +1,23 @@
-public class JwtAuthenticationManager implements ReactiveAuthenticationManager {
+public class JwtAuthenticationFilter extends AbstractAuthenticationFilter {
+
+    private final ReactiveAuthenticationManager authenticationManager;
+
+    public JwtAuthenticationFilter(ReactiveAuthenticationManager authenticationManager) {
+        super(authenticationManager);
+        this.authenticationManager = authenticationManager;
+    }
 
     @Override
-    public Mono<Authentication> authenticate(Authentication authentication) {
-        if (authentication instanceof JwtAuthenticationToken) {
-            JwtAuthenticationToken jwtAuthenticationToken = (JwtAuthenticationToken) authentication;
-            String token = jwtAuthenticationToken.getToken();
-
-            if (isValidToken(token)) {
-                // Extraer información del token y construir el objeto de autenticación
-                // Puedes usar una biblioteca como jjwt para validar y decodificar el token
-                // y luego construir un objeto de autenticación con los detalles necesarios
-
-                // Ejemplo:
-                String username = extractUsernameFromToken(token);
-                List<GrantedAuthority> authorities = extractAuthoritiesFromToken(token);
-
-                UserDetails userDetails = new User(username, "", authorities);
-                return Mono.just(new UsernamePasswordAuthenticationToken(userDetails, token, authorities));
-            } else {
-                return Mono.empty(); // Token inválido, la autenticación falla
-            }
-        }
-
-        return Mono.empty(); // No se puede autenticar con otros tipos de Authentication
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        return super.filter(exchange, chain);
     }
 
-    private boolean isValidToken(String token) {
-        // Lógica para validar la firma y la validez del token JWT
-        // Puedes usar una biblioteca como jjwt para realizar estas validaciones
-        // y devolver true si el token es válido, o false si no lo es
+    @Override
+    protected Mono<Authentication> filterInternal(ServerWebExchange exchange) {
+        return ServerHttpBearerAuthenticationConverter
+                .authenticate(exchange)
+                .flatMap(authentication -> authenticationManager.authenticate(authentication))
+                .switchIfEmpty(Mono.empty());
     }
 
-    private String extractUsernameFromToken(String token) {
-        // Lógica para extraer el nombre de usuario del token JWT
-        // Puedes usar una biblioteca como jjwt para decodificar el token y obtener el nombre de usuario
-    }
-
-    private List<GrantedAuthority> extractAuthoritiesFromToken(String token) {
-        // Lógica para extraer los roles o autorizaciones del token JWT
-        // Puedes usar una biblioteca como jjwt para decodificar el token y obtener los roles o autorizaciones
-    }
 }
